fcfaae8b74e8b507b09babbe9518f8272ff29468
Laura Kimble <laura@geoffreys-mbp.lan>
2020-01-05 21:07:16 -0700

    plotting code

---
+++ src/plots.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+%matplotlib inline
+import pandas as pd
+plt.style.use('ggplot')
+
+
+def plot_hist(df, col_name, view_by):
+    fig, ax = plt.subplots()
+
+    true_vals = df[df[view_by]=='True'][col_name]
+    false_vals = df[df[view_by]=='False'][col_name]
+
+    ax.hist(true_vals, color='blue', label='True')
+    ax.hist(false_vals, color='red', label='False')
+
+    ax.set_title(f'{col_name} for {view_by}')
+    ax.legend()
+
+
+if __name__ == '__main__':
+
+    # Convert all spark df's to pandas df's for plotting
+    businesses = business_df_flat_subset.select('*').toPandas()
+    users = user_df.select('*').toPandas()
+    reviews = review_df.select('*').toPandas()
