493cc6d109e88a71983f2b0fbfa1bf8c3b6fea4f
Laura Kimble <laura@Geoffreys-MacBook-Pro.local>
2020-01-06 15:12:46 -0700

    More plotting functions

--- src/business_df.py
+++ src/business_df.py
@@ -1,34 +1,105 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-plt.style.use('fivethirtyeight')
+plt.style.use('ggplot')
+plt.rcParams.update({'font.size': 14})
+
 
 class BusinessDF(pd.DataFrame):
     def __init__(self, df):
         super().__init__(df)
 
 
-    def plot_hist(self, col_name, view_by_col=''):
-        ''' Plot histrograms of the given col_name, one for each value of the view_by_col
+    def plot_value_counts_bar(self, col_name, save=False):
+        ''' 
+        Plot a bar chart of the counts of each value in the specified column (limited to 10 top values).
+
+        Parameters:
+            col_name (string): Name of a categorical column in the dataframe to plot the counts of values.
+            save (boolean): If true, will save the figure as a png file in the images folder.
+        ''' 
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+        data = self[col_name].value_counts()[0:10]
+        labels = data.index
+        N = len(labels)
+        tickLocations = np.arange(N)
+        
+        ax.barh(tickLocations, data)
+        ax.set_yticks(ticks=tickLocations)
+        ax.set_yticklabels(labels)
+        title = f'{col_name}: Value counts'
+        ax.set_title(title)
+        plt.gca().invert_yaxis()
+
+        if save:
+            fig.savefig(f'../images/{title}.png')
+        
+
+    def plot_stars_hist(self, view_by_col='', limit=10, save=False):
+        '''
+        Plot histrograms of avg star ratings, one plot for each value of the view_by_col
+
+        Parameters:
+            view_by_col (string): name of column to segment the data; shows 1 plot for each value
+            limit (int): Max number of plots to show (will show top x frequent unique values of view_by_col)
+            save (boolean): If true, will save the figure as a png file in the images folder.
         '''
         if view_by_col:
-            num_plots = len(self[view_by_col].unique())
-            fig, axs = plt.subplots(num_plots, 1, figsize=(10, 4))
+            labels = self[view_by_col].value_counts()[0:limit].index  # Get the top x most frequent values of the view_by_col
+            num_plots = len(labels)
+            fig, axs = plt.subplots(num_plots, 1, sharex=True, figsize=(8, 4 * num_plots))
 
-            for idx, val in enumerate(self[view_by_col].unique()):
-                data = self[self[view_by_col]==val][col_name]
+            for idx, label in enumerate(np.array(labels)):
+                data = self[self[view_by_col]==label]['stars']
                 ax = axs[idx]
-                ax.hist(data, label=f'{view_by_col} = {val}')
-                ax.set_title(f'{col_name}')
+                ax.hist(data, bins=8, label=f'{view_by_col} = {label}')
+                ax.set_title('Average Star Ratings')
                 ax.legend()
 
         else:
-            fig, ax = plt.subplots(1, 1, figsize=(4, 8))
-            ax.hist(self[col_name], label=f'{col_name}')
-            ax.legend()
+            fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+            ax.hist(self['stars'], bins=8)
+            ax.set_title('Average Star Ratings')
 
-        plt.tight_layout()
+        plt.tight_layout(pad=2)
+
+        if save:
+            fig.savefig(f'../images/Stars distribution by {view_by_col}.png')
 
 
-if __name__ == '__main__':
-    businesses = BusinessDF(businesses)
+    def plot_review_counts_hist(self, view_by_col='', cutoff=5000, save=False):
+        ''' 
+        Plot histrograms of review counts, one plot for each value of the view_by_col.
+
+        Parameters:
+            view_by_col (string): name of column to segment the data; shows 1 plot for each value
+            cutoff (int): Only include businesses with fewer reviews than the cutoff in the plot, for viewability.
+            save (boolean): If true, will save the figure as a png file in the images folder.
+        '''
+        cutoff_data = self[self['review_count'] < cutoff]
+        
+        if view_by_col:
+            num_plots = len(cutoff_data[view_by_col].unique())
+            fig, axs = plt.subplots(num_plots, 1, sharex=True, figsize=(8, 4 * num_plots))
+
+            for idx, val in enumerate(cutoff_data[view_by_col].unique()):
+                data = cutoff_data[cutoff_data[view_by_col]==val]['review_count']
+                ax = axs[idx]
+                ax.hist(data, bins=20, label=f'{view_by_col} = {val}')
+                ax.set_title('Review Counts')
+                ax.legend()
+
+        else:
+            fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+            ax.hist(cutoff_data['review_count'], bins=20)
+            ax.set_title('Review Counts')
+
+        plt.tight_layout(pad=2)
+
+        if save:
+            fig.savefig(f'../images/Review Counts by {view_by_col}.png')
+
+
+# if __name__ == '__main__':
+#     businesses = BusinessDF(businesses)
