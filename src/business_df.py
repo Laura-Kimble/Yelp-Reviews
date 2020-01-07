04e652121c4cb125c4239d1f70cb43c300b2bf81
Laura Kimble <laura@Geoffreys-MacBook-Pro.local>
2020-01-07 09:22:29 -0700

    filter_by arg for plots

--- src/business_df.py
+++ src/business_df.py
@@ -10,60 +10,85 @@ class BusinessDF(pd.DataFrame):
         super().__init__(df)
 
 
-    def plot_value_counts_bar(self, col_name, save=False):
+    def plot_value_counts_bar(self, col_name, filter_by=(), save=False):
         ''' 
         Plot a bar chart of the counts of each value in the specified column (limited to 10 top values).
 
         Parameters:
             col_name (string): Name of a categorical column in the dataframe to plot the counts of values.
+            filter_by (tuple): Used to filter the data for plotting, e.g., filter_by=('Restaurant', [True]) will
+                only plot businesses where the Restaurant column=True.  First value of the filter_by tuple is the
+                column name (string) to filter on; second value is a list of values to keep.
             save (boolean): If true, will save the figure as a png file in the images folder.
-        ''' 
+        '''
+
+        if filter_by:
+            col, vals = filter_by
+            filtered = self[self[col].isin(vals)]
+            legend_label = f'count of businesses where {col} is in {vals}'
+        else:
+            filtered = self
+            legend_label = 'count of businesses'
 
         fig, ax = plt.subplots(1, 1, figsize=(10, 6))
-        data = self[col_name].value_counts()[0:10]
+        data = filtered[col_name].value_counts()[0:10]
         labels = data.index
         N = len(labels)
         tickLocations = np.arange(N)
         
-        ax.barh(tickLocations, data)
+        ax.barh(tickLocations, data, label=legend_label)
         ax.set_yticks(ticks=tickLocations)
         ax.set_yticklabels(labels)
+        ax.set_xlabel('number of businesses')
+        ax.set_ylabel(f'{col_name}')
         title = f'Number of Businesses by {col_name}'
         ax.set_title(title)
+        ax.legend()
         plt.gca().invert_yaxis()
 
         if save:
             fig.savefig(f'../images/{title}.png')
         
 
-    def plot_stars_hist(self, view_by_col='', limit=10, save=False):
+    def plot_stars_hist(self, view_by_col='', filter_by=(), limit=10, save=False):
         '''
         Plot histrograms of avg star ratings, one plot for each value of the view_by_col
 
         Parameters:
             view_by_col (string): name of column to segment the data; shows 1 plot for each value
+            filter_by (tuple): Used to filter the data for plotting, e.g., filter_by=('Restaurant', [True]) will
+                only plot businesses where the Restaurant column=True.  First value of the filter_by tuple is the
+                column name (string) to filter on; second value is a list of values to keep.
             limit (int): Max number of plots to show (will show top x frequent unique values of view_by_col)
             save (boolean): If true, will save the figure as a png file in the images folder.
         '''
+
+        if filter_by:
+            col, vals = filter_by
+            filtered = self[self[col].isin(vals)]
+            legend_label = f'star ratings where {col} is in {vals}'
+        else:
+            filtered = self
+            legend_label = 'star ratings'
         
         if view_by_col:
-            labels = self[view_by_col].value_counts()[0:limit].index  # Get the top x most frequent values of the view_by_col
+            labels = filtered[view_by_col].value_counts()[0:limit].index  # Get the top x most frequent values of the view_by_col
             num_plots = len(labels)
             fig, axs = plt.subplots(num_plots, 1, sharex=True, figsize=(8, 4 * num_plots))
 
             for idx, label in enumerate(np.array(labels)):
-                data = self[self[view_by_col]==label]['stars']
+                data = filtered[filtered[view_by_col]==label]['stars']
                 ax = axs[idx]
-                ax.hist(data, bins=8, label=f'{view_by_col} = {label}')
+                ax.hist(data, bins=8, label=legend_label)
                 ax.set_xlabel('avg. star rating')
                 ax.set_title(f'{label}')
-                # ax.legend()
+                ax.legend()
 
             title = f'Star Ratings by {view_by_col}'
 
         else:
             fig, ax = plt.subplots(1, 1, figsize=(8, 4))
-            ax.hist(self['stars'], bins=8)
+            ax.hist(filtered['stars'], bins=8)
             title = 'Star Ratings Overall'
             ax.set_title(title)
 
@@ -73,36 +98,47 @@ class BusinessDF(pd.DataFrame):
             fig.savefig(f'../images/{title}.png')
 
 
-    def plot_review_counts_hist(self, view_by_col='', limit=10, cutoff=5000, save=False):
+    def plot_review_counts_hist(self, view_by_col='', filter_by=(), limit=10, cutoff=5000, save=False):
         ''' 
         Plot histrograms of review counts, one plot for each value of the view_by_col.
 
         Parameters:
             view_by_col (string): name of column to segment the data; shows 1 plot for each value
+            filter_by (tuple): Used to filter the data for plotting, e.g., filter_by=('Restaurant', [True]) will
+                only plot businesses where the Restaurant column=True.  First value of the filter_by tuple is the
+                column name (string) to filter on; second value is a list of values to keep.
             limit (int): Max number of plots to show (will show top x frequent unique values of view_by_col)
             cutoff (int): Only include businesses with fewer reviews than the cutoff in the plot, for viewability.
             save (boolean): If true, will save the figure as a png file in the images folder.
         '''
         cutoff_data = self[self['review_count'] < cutoff]
+
+        if filter_by:
+            col, vals = filter_by
+            filtered = cutoff_data[cutoff_data[col].isin(vals)]
+            legend_label = f'review counts where {col} is in {vals}'
+        else:
+            filtered = self
+            legend_label = 'review counts'
         
         if view_by_col:
-            labels = cutoff_data[view_by_col].value_counts()[0:limit].index  # Get the top x most frequent values of the view_by_col
+            labels = filtered[view_by_col].value_counts()[0:limit].index  # Get the top x most frequent values of the view_by_col
             num_plots = len(labels)
             fig, axs = plt.subplots(num_plots, 1, sharex=True, figsize=(8, 4 * num_plots))
 
             for idx, label in enumerate(np.array(labels)):
-                data = cutoff_data[cutoff_data[view_by_col]==label]['review_count']
+                data = filtered[filtered[view_by_col]==label]['review_count']
                 ax = axs[idx]
-                ax.hist(data, bins=20, label=f'{view_by_col} = {label}')
+                ax.hist(data, bins=20, label=legend_label)
                 ax.set_xlabel('review counts')
                 ax.set_title(f'{label}')
-                # ax.legend()
+                ax.legend()
 
             title = f'Review Counts by {view_by_col}'
 
         else:
             fig, ax = plt.subplots(1, 1, figsize=(8, 4))
-            ax.hist(cutoff_data['review_count'], bins=20)
+            ax.hist(filtered['review_count'], bins=20)
             title = 'Review Counts Overall'
             ax.set_title(title)
 
