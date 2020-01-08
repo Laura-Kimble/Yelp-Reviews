b30680ab542d38696e1a87ae5397c88ec2bfa720
Laura Kimble <laura@Geoffreys-MacBook-Pro.local>
2020-01-08 14:24:07 -0700

    clean map code

--- src/mapping.py
+++ src/mapping.py
@@ -4,37 +4,7 @@ import datetime
 import folium
 from folium.plugins import HeatMap
 
-#Load the pickeled dataframes
-businesses_df = pd.read_pickle('../data/pickled_businesses_df')
-category_counts = pd.read_pickle('../data/pickled_category_counts')
-
-top_4_cats = list(category_counts['elem'][0:4])
-
-# Set up variables for Vegas map
-vegas_df = businesses_df[businesses_df['city']=='Las Vegas']
-num_vegas_businesses = len(vegas_df)
-default_max_val = 30  # default value to use as maximum point intensity in the heatmap
-
-
-# Set up Charlotte map data
-charlotte_df = businesses_df[businesses_df['city']=='Charlotte']
-
-base_map = folium.Map(location=[39.73782,-104.971338],
-                        zoom_start=4,
-                        tiles="Cartodbpositron")
-
-
-vegas_map = folium.Map(location=[36.1699, -115.1398],
-                        zoom_start=11,
-                        tiles="Cartodbpositron")
-
-
-charlotte_map = folium.Map(location=[35.2271, -80.8431],
-                        zoom_start=11,
-                        tiles="Cartodbpositron")
-
-
-def create_heatmap_layer(df, base_map, layer_name, max_val=default_max_val):
+def create_heatmap_layer(df, base_map, layer_name):
     '''
     Create a heatmap layer onto the base map, with the lat/lon in the dataframe.
 
@@ -42,15 +12,13 @@ def create_heatmap_layer(df, base_map, layer_name, max_val=default_max_val):
         df (dataframe): Data to show in the heatmap layer, with 'latitude' and 'longitude' columns
         base_map (folium Map object): Base map to apply the layer to
         layer_name (str): Name of the layer.
-        max_val (double): Will be passed into HeatMap for the maximum point intensity, used to increase intensity
-            in df's with few number of rows.
     '''
 
     feature_map = folium.FeatureGroup(name = layer_name)
     max_amount = float(max_val)
     feature_map.add_child(HeatMap(list(zip(df['latitude'].values, df['longitude'].values)), 
                             min_opacity=0.2,
-                            max_val=max_amount,
+                            max_val=30,
                             radius=4, blur=1, 
                             max_zoom=1
                         ))
@@ -98,26 +66,44 @@ def create_dots_layer(df, base_map, layer_name, color='black'):
 
 if __name__ == '__main__':
 
+    #Load the pickeled dataframes
+    businesses_df = pd.read_pickle('../data/pickled_businesses_df')
+    category_counts = pd.read_pickle('../data/pickled_category_counts')
+    top_4_cats = list(category_counts['elem'][0:4])
+
+    # Set up city dataframes for mapping
+    vegas_df = businesses_df[businesses_df['city']=='Las Vegas']
+    charlotte_df = businesses_df[businesses_df['city']=='Charlotte']
+
+    base_map = folium.Map(location=[39.73782,-104.971338],
+                            zoom_start=4,
+                            tiles="Cartodbpositron")
+
+
+    vegas_map = folium.Map(location=[36.1699, -115.1398],
+                            zoom_start=11,
+                            tiles="Cartodbpositron")
+
+
+    charlotte_map = folium.Map(location=[35.2271, -80.8431],
+                            zoom_start=11,
+                            tiles="Cartodbpositron")
+
+
     # Create overall heatmap for all businesses in dataset
     create_heatmap_layer(businesses_df, base_map, 'all businesses')
 
-    # Create map layers for Vegas
+    # Create heatmap layers for Vegas
     create_heatmap_layer(vegas_df, vegas_map, 'all businesses')
 
     vegas_five_stars_df = vegas_df[vegas_df['stars']==5.0]
-    #max_val = (len(five_stars_df) / len(vegas_df)) * default_max_val * 2
     create_heatmap_layer(vegas_five_stars_df, vegas_map, '5-star businesses')
 
     chinese_df = vegas_df[vegas_df['categories'].str.contains(pat='Chinese')]
-    #max_val = (len(chinese_df) / num_vegas_businesses) * default_max_val * 2
     create_heatmap_layer(chinese_df, vegas_map, 'Chinese restaurants')
 
 
     # Create map layers for Charlotte
-    # create_dots_layer(charlotte_df, charlotte_map, 'all businesses', 'black')
-
-    # charlotte_restaurants_df = charlotte_df[charlotte_df['Restaurant']==True]
-    # create_dots_layer(charlotte_restaurants_df, charlotte_map, 'Restaurants', 'red')
     create_multiple_dots_layers(charlotte_df, charlotte_map, top_4_cats, 'categories')
 
     # add toggle controls for the layers
